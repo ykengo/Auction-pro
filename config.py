5485ff21440f15416fab91b66267998c32d47338
Rodion Zhuravlev <zuravlevrodion55@gmail.com>
2025-03-25 16:59:38 +0500

    Сделал добавление лота и просмотр всех лотов

--- config.py
+++ config.py
@@ -4,3 +4,5 @@
 TOKEN = '7830327936:AAGf7PYlCCfC2L09bDJhCOy2oJnY0prsnbQ'
 LINK_BOT = 't.me/AuctionRussiaBot'
 DATA_BASE = ''
+DOWNLOADS_USERS_LOT = 'image users lot'
+
--- utils/state_machine.py
+++ utils/state_machine.py
@@ -6,11 +6,19 @@ from aiogram.fsm.state import State, StatesGroup
 
 class StateCreateLot(StatesGroup):
 
-    '''Класс для описания поведения ТГ бота в момент создания лота'''
+    """Класс для описания поведения ТГ бота в момент создания лота"""
 
     NAME_LOT = State()  # Название лота
     TEXT_LOT = State()  # Описание лота
     IMAGES_LOT = State()  # Изображения лота
 #   TAGS_LOT = State()  # Теги лота (Пока под вопросом)
     DATE_LOT = State()  # Дата начала и конца лота
+    START_PRICE = State()
+    STEP = State()
+
+class StateAllLots(StatesGroup):
+
+    """Класс для описания поведения ТГ бота в момент просмотра лотов пользователей"""
+
+    WATCHING = State()  # В категории просматривает
 
